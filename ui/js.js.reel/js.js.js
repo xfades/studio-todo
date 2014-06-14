a75b5a82a69fde1e619e128a88f60166361c116e
xfades <noreply>
2014-06-14 22:29:34 +0000

    Add component js.js

---
+++ ui/js.js.reel/js.js.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/js.js.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Js.js
+ * @extends Component
+ */
+exports.Js.js = Component.specialize(/** @lends Js.js# */ {
+    constructor: {
+        value: function Js.js() {
+            this.super();
+        }
+    }
+});
